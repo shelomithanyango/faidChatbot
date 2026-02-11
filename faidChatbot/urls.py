f6a02da56f31e00c516c07fefec47d3cb945ef94
Shelomith Anyango <158587536+shelomithanyango@users.noreply.github.com>
2026-02-12 01:38:49 +0300

    Fix include syntax for homepage.urls

--- faidChatbot/urls.py
+++ faidChatbot/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('',include('homepage.urls'),
+    path('',include('homepage.urls')),
     path('admin/', admin.site.urls),
     path("chat/", include("chat.urls")),
     path('auth/', include('authentication.urls')),
