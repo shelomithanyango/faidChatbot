005f96b2dbe07b1229495db394c8c3f46ada0032
Shelomith Anyango <shelomith42@gmail.com>
2026-06-08 23:47:14 +0300

    changed settings.py

--- faidChatbot/wsgi.py
+++ faidChatbot/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for faidChatbot project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling # 👈 Implements Vercel file routing handlers
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'faidChatbot.settings')
 
-application = get_wsgi_application()
+# Wrap the application so it serves static assets and media photo directories cleanly
+application = Cling(MediaCling(get_wsgi_application()))
+
+# Global gateway hook used directly by Vercel deployment containers
+app = application
